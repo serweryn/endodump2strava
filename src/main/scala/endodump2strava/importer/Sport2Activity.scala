abf0a2bf5dba4e75482d1a7a06e8a2796629b53f
Seweryn Sobieszek <serweryn@gmail.com>
2021-01-23 20:18:14 +0100

    get rid of warnings

--- src/main/scala/endodump2strava/importer/Sport2Activity.scala
+++ src/main/scala/endodump2strava/importer/Sport2Activity.scala
@@ -9,34 +9,34 @@ object Sport2Activity {
   def apply(sport: Sport): ActivityParams = mapping(sport)
 
   private val mapping: Map[Sport, ActivityParams] = Map(
-    CROSSFIT               -> ActivityParams(Crossfit, false),
-    CYCLING_SPORT          -> ActivityParams(Ride, false),
-    CYCLING_TRANSPORTATION -> ActivityParams(Ride, true),
-    FITNESS_WALKING        -> ActivityParams(Walk, false),
-    HIKING                 -> ActivityParams(Hike, false),
-    ICE_SKATING            -> ActivityParams(IceSkate, false),
-    KAYAKING               -> ActivityParams(Kayaking, false),
-    KICK_SCOOTER           -> ActivityParams(Skateboard, false),
-    ORIENTEERING           -> ActivityParams(Run, false),
-    OTHER                  -> ActivityParams(Workout, false),
-    PILATES                -> ActivityParams(Yoga, false),
-    RIDING                 -> ActivityParams(Workout, false),
-    ROLLER_SKATING         -> ActivityParams(InlineSkate, false),
-    RUNNING                -> ActivityParams(Run, false),
-    SAILING                -> ActivityParams(Sail, false),
-    SKIING_DOWNHILL        -> ActivityParams(AlpineSki, false),
-    SPINNING               -> ActivityParams(Ride, false, true),
-    STRETCHING             -> ActivityParams(Yoga, false),
-    SWIMMING               -> ActivityParams(Swim, false),
-    TREADMILL_RUNNING      -> ActivityParams(Run, false, true),
-    WALKING                -> ActivityParams(Walk, false),
-    WEIGHT_TRAINING        -> ActivityParams(WeightTraining, false),
-    YOGA                   -> ActivityParams(Yoga, false)
+    CROSSFIT               -> ActivityParams(Crossfit),
+    CYCLING_SPORT          -> ActivityParams(Ride),
+    CYCLING_TRANSPORTATION -> ActivityParams(Ride, commute = true),
+    FITNESS_WALKING        -> ActivityParams(Walk),
+    HIKING                 -> ActivityParams(Hike),
+    ICE_SKATING            -> ActivityParams(IceSkate),
+    KAYAKING               -> ActivityParams(Kayaking),
+    KICK_SCOOTER           -> ActivityParams(Skateboard),
+    ORIENTEERING           -> ActivityParams(Run),
+    OTHER                  -> ActivityParams(Workout),
+    PILATES                -> ActivityParams(Yoga),
+    RIDING                 -> ActivityParams(Workout),
+    ROLLER_SKATING         -> ActivityParams(InlineSkate),
+    RUNNING                -> ActivityParams(Run),
+    SAILING                -> ActivityParams(Sail),
+    SKIING_DOWNHILL        -> ActivityParams(AlpineSki),
+    SPINNING               -> ActivityParams(Ride, trainer = true),
+    STRETCHING             -> ActivityParams(Yoga),
+    SWIMMING               -> ActivityParams(Swim),
+    TREADMILL_RUNNING      -> ActivityParams(Run, trainer = true),
+    WALKING                -> ActivityParams(Walk),
+    WEIGHT_TRAINING        -> ActivityParams(WeightTraining),
+    YOGA                   -> ActivityParams(Yoga)
   )
 }
 
 final case class ActivityParams(
   activityType: ActivityType,
-  commute: Boolean,
+  commute: Boolean = false,
   trainer: Boolean = false
 )
