9262d820938da3b275a3eb83ad533c69d7679783
Seweryn Sobieszek <serweryn@gmail.com>
2021-01-29 22:56:55 +0100

    use Akka execution context

--- src/main/scala/endodump2strava/importer/Main.scala
+++ src/main/scala/endodump2strava/importer/Main.scala
@@ -2,13 +2,14 @@ package endodump2strava.importer
 
 import akka.actor.ActorSystem
 
-import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.duration.DurationInt
+import scala.concurrent.ExecutionContext
 
 object Main extends App {
 
-  private implicit val system: ActorSystem = ActorSystem()
-  private val importer = new Importer()
+  implicit val system: ActorSystem = ActorSystem()
+  implicit val ec: ExecutionContext = system.dispatcher
+  val importer = new Importer()
 
   val importTask = system.scheduler.schedule(0 seconds, 15 minutes)(importer.doImport())
   system.registerOnTermination(importTask.cancel())
