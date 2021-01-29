c37fcb964055c398789b2ec537cc9c3eac9ce427
Seweryn Sobieszek <serweryn@gmail.com>
2021-01-29 21:34:58 +0100

    invoker which tracks reported errors and invalidates itself when certain errors occur

---
+++ src/main/scala/endodump2strava/importer/GuardedApiInvoker.scala
@@ -0,0 +1,37 @@
+package endodump2strava.importer
+
+import io.swagger.client.core.{ApiError, ApiInvoker, ApiRequest, ApiResponse}
+
+import java.util.concurrent.atomic.AtomicBoolean
+import scala.concurrent.{ExecutionContextExecutor, Future}
+
+class GuardedApiInvoker(invoker: ApiInvoker) {
+
+  private implicit val ec: ExecutionContextExecutor = invoker.ec
+
+  private val invalidateOnCodes = Seq(401, 429)
+
+  val invalidCode = 499
+  private val invalidMsg = "You cannot make any more requests now, please try again in 15 minutes"
+
+  private[this] val stillValid = new AtomicBoolean(true)
+
+  def execute[T](r: ApiRequest[T]): Future[ApiResponse[T]] = {
+    if (!valid) respondWithInvalid()
+    else {
+      // execute real request and invalidate this invoker if got specific error response
+      val f = invoker.execute(r)
+      f recoverWith {
+        case ApiError(code, _, _, _, _) =>
+          if (invalidateOnCodes.contains(code)) stillValid.set(false)
+          f
+      }
+    }
+  }
+
+  def valid: Boolean = stillValid.get()
+
+  private def respondWithInvalid[T](): Future[ApiResponse[T]] =
+    Future.failed(ApiError(invalidCode, invalidMsg, None))
+
+}
