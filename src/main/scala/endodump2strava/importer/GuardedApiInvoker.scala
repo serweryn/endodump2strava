7e9e87ce2343138380c00fb793503a691164b207
Seweryn Sobieszek <serweryn@gmail.com>
2021-02-04 08:30:06 +0100

    make 100 requests guard

--- src/main/scala/endodump2strava/importer/GuardedApiInvoker.scala
+++ src/main/scala/endodump2strava/importer/GuardedApiInvoker.scala
@@ -2,7 +2,7 @@ package endodump2strava.importer
 
 import io.swagger.client.core.{ApiError, ApiInvoker, ApiRequest, ApiResponse}
 
-import java.util.concurrent.atomic.AtomicBoolean
+import java.util.concurrent.atomic.AtomicInteger
 import scala.concurrent.{ExecutionContextExecutor, Future}
 
 class GuardedApiInvoker(invoker: ApiInvoker) {
@@ -10,28 +10,29 @@ class GuardedApiInvoker(invoker: ApiInvoker) {
   private implicit val ec: ExecutionContextExecutor = invoker.ec
 
   private val invalidateOnCodes = Seq(401, 429)
+  private val maxRequests = 100
 
   val invalidCode: Int = ErrorCodes.InvalidApiConnection
   val invalidMsg = "You cannot make any more requests now, please try again in 15 minutes"
 
-  private[this] val stillValid = new AtomicBoolean(true)
+  private[this] val requestsToMake = new AtomicInteger(maxRequests)
 
   def execute[T](r: ApiRequest[T]): Future[ApiResponse[T]] = {
-    if (!valid) respondWithInvalid()
+    if (requestsToMake.getAndDecrement() <= 0) respondWithInvalid()
     else {
       // execute real request and invalidate this invoker if got specific error response
       val f = invoker.execute(r)
       f recoverWith {
         case ApiError(code, _, _, _, _) =>
-          if (invalidateOnCodes.contains(code)) stillValid.set(false)
+          if (invalidateOnCodes.contains(code)) requestsToMake.set(0)
           f
       }
     }
   }
 
-  def valid: Boolean = stillValid.get()
+  def valid: Boolean = requestsToMake.get() > 0
 
-  def reset(): Unit = stillValid.set(true)
+  def reset(): Unit = requestsToMake.set(maxRequests)
 
   private def respondWithInvalid[T](): Future[ApiResponse[T]] =
     Future.failed(ApiError(invalidCode, invalidMsg, None))
